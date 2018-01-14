d34b11dc3c909834dd36653abe938ee1a5807d4f
Damian Kreick <damian.kreick@gmail.com>
2018-01-14 20:25:23 -0300

    Adds Renault4, Implements car movement

---
+++ Ride the Dacia/Assets/Scripts/CarManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarManager : MonoBehaviour {
+
+	public float speed;
+
+	void Update ()
+	{
+		MovePlayer ();
+	}
+
+	void MovePlayer()
+	{
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			transform.Translate(Vector3.left * (speed * Time.deltaTime));
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector3.right * (speed * Time.deltaTime));
+		}
+	}
+}
---
+++ Ride the Dacia/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+	
+	public void ChangeScene(string sceneName)
+	{
+		
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+}
